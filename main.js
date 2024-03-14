53f083cfff02598828ade66b73177b3f41bdb2d4
reduviidae <wiljago@gmail.com>
2024-03-13 18:10:04 -0700

    manifest and main file created

--- main.js
+++ main.js
@@ -6,6 +6,22 @@ for (let element of inPlayItemList) {
 }
 
 console.log(arr);
+console.log("hey this shit is working as expected")
+const { fetch: originalFetch } = window;
+
+window.fetch = async (...args) => {
+    let [resource, config ] = args;
+  
+    // request interceptor here
+    const response = await originalFetch(resource, config);
+    // response interceptor here
+    console.log(response);
+};
+
+document.addEventListener("fetch", function(event) {
+    console.log("resource", event);
+}, true);
+
 
 // Evan discovered that there is a hidden Water tile that populates on a fresh screen.
 // This application should click on the broom to clear the screen first.
